a2b80c465cfd3b52f240dfe4ee1f5688cf5ec34a
AndoYu416 <yu,ando.v416@dirbato.co.jp>
2024-04-08 12:35:26 +0900

    second commit

---
+++ Sample/src/MethodSample.java
@@ -0,0 +1,20 @@
+
+public class MethodSample {
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 10;
+
+        sumMethod1(number1, number2);
+    
+        int result2 = sumMethod1(number1, number2);
+        System.out.println("sumMethod2の結果は" + result2);
+    }    
+    public static int sumMethod1(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println("sumMethod1の結果は" + result);
+        return result;
+
+    }
+
+}
